2b0d230eab2ece2437c6636ce750450fac7c4bb6
Worawat Kaewsanmueng <lottett@ttoeys-MBA.local>
2025-03-08 10:13:16 +0700

    Task 1 init

---
+++ src/views/eventListView.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'EventListView',
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    // Fetch events or perform other setup tasks here
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>Event List</h1>
+    <ul>
+      <li v-for="event in events" :key="event.id">{{ event.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+/* Add your styles here */
+</style>
