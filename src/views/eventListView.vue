f348b898fb864451eab3463db0976afd53d4c794
Worawat Kaewsanmueng <lottett@ttoeys-MBA.local>
2025-03-08 11:37:00 +0700

    Task 3 v-directive, v-if, v-for และ ref object

--- src/views/eventListView.vue
+++ src/views/eventListView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import EventCard from '@/components/eventCard.vue'
+
+const events = ref([
+  { id: 1, title: 'Event 1', date: '2021-07-01' },
+  { id: 2, title: 'Event 2', date: '2021-07-02' },
+  { id: 3, title: 'Event 3', date: '2021-07-03' },
+])
 </script>
 
 <template>
-  <EventCard />
-  <EventCard />
-  <EventCard />
-  <EventCard />
-  <EventCard />
+  <div>
+    <EventCard v-for="event in events" :key="event.id" />
+  </div>
 </template>
