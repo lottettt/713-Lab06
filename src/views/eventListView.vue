2013285e088d62fab8ece829c0946a7e2bb08c4b
Worawat Kaewsanmueng <lottett@ttoeys-MBA.local>
2025-03-08 13:38:48 +0700

    Task 4 using props injection

--- src/views/eventListView.vue
+++ src/views/eventListView.vue
@@ -3,14 +3,58 @@ import { ref } from 'vue'
 import EventCard from '@/components/eventCard.vue'
 
 const events = ref([
-  { id: 1, title: 'Event 1', date: '2021-07-01' },
-  { id: 2, title: 'Event 2', date: '2021-07-02' },
-  { id: 3, title: 'Event 3', date: '2021-07-03' },
+  {
+    id: 1,
+    category: 'Category 1',
+    title: 'Event 1',
+    description: 'Description of Event 1',
+    date: '2021-10-10',
+    time: '10:00',
+    location: 'Location 1',
+    petsAllowed: true,
+    organizer: {
+      name: 'Organizer 1',
+    },
+  },
+  {
+    id: 2,
+    category: 'Category 2',
+    title: 'Event 2',
+    description: 'Description of Event 2',
+    date: '2021-10-11',
+    time: '11:00',
+    location: 'Location 2',
+    petsAllowed: false,
+    organizer: {
+      name: 'Organizer 2',
+    },
+  },
+  {
+    id: 3,
+    category: 'Category 3',
+    title: 'Event 3',
+    description: 'Description of Event 3',
+    date: '2021-10-12',
+    time: '12:00',
+    location: 'Location 3',
+    petsAllowed: true,
+    organizer: {
+      name: 'Organizer 3',
+    },
+  },
 ])
 </script>
 
 <template>
-  <div>
-    <EventCard v-for="event in events" :key="event.id" />
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
