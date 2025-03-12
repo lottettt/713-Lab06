cdb974c782a9d6a196e8bea157fa7a0bd06eb82b
Worawat Kaewsanmueng <lottett@ttoeys-MBA.local>
2025-03-12 22:52:37 +0700

    add eventDetailView.vue

---
+++ src/views/eventDetailView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Event } from '@/types.ts'
+const event = ref<Event>()
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>(( event.name ))</h1>
+    <p>(( event.time )) on (( event.date )) @ (( event.location ))</p>
+    <p>(( event.description ))</p>
+  </div>
+  <div v-else>
+    <p>Loading . . .</p>
+  </div>
+</template>
