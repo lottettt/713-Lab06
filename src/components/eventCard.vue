c81f2b35d86a89097878f11d8adc06b84fa00d6f
lottettt <lotte.worawat@gmail.com>
2025-03-14 14:38:18 +0700

    ปรับ EventCard ให้สวยขึ้น

--- src/components/eventCard.vue
+++ src/components/eventCard.vue
@@ -4,7 +4,7 @@ defineProps<{ event: Event }>()
 </script>
 
 <template>
-  <RouterLink :to="{ name: 'event-detail-view', params: { id: event.id } }">
+  <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <p>{{ event.description }}</p>
@@ -25,4 +25,10 @@ defineProps<{ event: Event }>()
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
+
+.event-link {
+  text-decoration: none;
+  color: inherit;
+}
+
 </style>
